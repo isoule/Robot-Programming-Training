82819b82078b54ddeec20d44624461633de6f53f
Ionatan Soule <ionatan98@gmail.com>
2016-11-21 10:10:45 -0800

    Finished all h1 problems

--- homework1/problem3/robot.cpp
+++ homework1/problem3/robot.cpp
@@ -2,13 +2,29 @@
 #include "robot.h"
 
 void Robot::drive(int distance) {
-	robotPosition += distance;
+	//Needed to limit robot movement to [0, 7]
+	//Not sure how else to do it for this assignment
+	if (robotPosition + distance >= 7) {
+		robotPosition = 7;
+	} else if (robotPosition + distance <= 0) {
+		robotPosition = 0;
+	} else {
+		robotPosition += distance;
+	}
 }
 void Robot::moveArmTo(int position) {
-	armPosition = position;
+	//Limit arm to [0, 10]. See above comment.
+	if (position >= 10) {
+		armPosition = 10;
+	} else if (position <= 0) {
+		armPosition = 0;
+	} else {
+		armPosition = position;
+	}
 }
 void Robot::scorePiece() {
-	if (getArmPosition() == 10 && getPosition() == 6 &&  hasPiece()) {
+	if (getArmPosition() == 10 && getPosition() == 6 && hasPiece()) {
+		//Scores piece -> no longer has piece
 		score += 1;
 		piece = false;
 	}
@@ -31,6 +47,8 @@ int Robot::getScore() {
 	return score;
 }
 
+//Prints robot status to console.
+//Needed a way to check to make sure the functions worked.
 void Robot::getStatus() {
 	std::cout << "Postion: " << robotPosition << std::endl;
 	std::cout << "Arm position: " << armPosition << std::endl;
