282cbac60e87b602f416733d2ad2094a6d68ef0e
isoule <ionatan98@gmail.com>
2016-12-06 21:51:08 -0800

    Changed variable name.

--- h3/p3/patron.h
+++ h3/p3/patron.h
@@ -1,11 +1,13 @@
 #ifndef PATRON_H_
 #define PATRON_H_
 
+#include "book.h"
+
 #include <vector>
 
 class Patron {
  public:
-  std::vector<Book> patron_;
+  std::vector<Book> patron_books_;
 };
 
 #endif // PATRON_H_
