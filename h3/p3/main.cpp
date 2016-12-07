a96e1694e1fe2c5281933774e762a39a394e9bed
isoule <ionatan98@gmail.com>
2016-12-06 21:16:01 -0800

    Began main.cpp. Need to work on library.(h/cpp).

---
+++ h3/p3/main.cpp
@@ -0,0 +1,14 @@
+#include "library.h"
+#include "patron.h"
+#include "book.h"
+
+int main() {
+  
+  //Build Library
+  Library Ion();
+
+  //Get user input
+  //With all of the options in library.h
+
+  return 0;
+}
