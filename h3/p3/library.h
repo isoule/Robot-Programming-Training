29013ae6ee4a1e89fc3194da220edef365d82c04
isoule <ionatan98@gmail.com>
2016-12-06 19:15:49 -0800

    Created task list for library

--- h3/p3/library.h
+++ h3/p3/library.h
@@ -11,7 +11,17 @@
 
 class Library {
  public:
+  //Need to keep track of all dem books
   std::vector<Book> library_;
+  //Need to keep track of all dem patrons
+  std::vector<Patron> patrons_;
+
+  //Need to be able to:
+  //Add books
+  //Add patrons
+  //Checkout/return books
+  //See what is available
+  //See who has checked out what
 };
 
 #endif  // LIBRARY_H_
