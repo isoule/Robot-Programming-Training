557fb123dd4ea85313e66a0e218e376bcce2073f
isoule <ionatan98@gmail.com>
2016-12-09 21:38:34 -0800

    bazel built h3/p1/*

--- h3/p1/main.cpp
+++ h3/p1/main.cpp
@@ -1,29 +1,29 @@
-#include <iostream> 
-#include <vector> 
-#include <algorithm> 
-#include <typeinfo> 
-#include <string> 
- 
-int main() {  
- 
-	std::vector<double> numbers; 
-	std::string input; 
- 
-	while (true) {  
-		std::cout << "Enter number: " << std::endl; 
-		std::getline(std::cin, input); 
-		if (input.length() == 0) { 
-                        break; 
-                } 
-		double num = std::stod(input);
-		numbers.push_back(num); 
-	}  
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <vector>
 
-	std::sort(std::begin(numbers), std::end(numbers));
+int main() {
 
-	for (auto i = std::begin(numbers); i != std::end(numbers); i++) {
-		std::cout << *i << std::endl;
-	}
+  std::vector<double> numbers;
+  std::string input;
 
-	return 0; 
+  while (true) {
+    std::cout << "Enter number: " << std::endl;
+    std::getline(std::cin, input);
+    if (input.length() == 0) {
+      break;
+    }
+    double num = std::stod(input);
+    numbers.push_back(num);
+  }
+
+  std::sort(std::begin(numbers), std::end(numbers));
+
+  for (auto i = std::begin(numbers); i != std::end(numbers); i++) {
+    std::cout << *i << std::endl;
+  }
+
+  return 0;
 }
